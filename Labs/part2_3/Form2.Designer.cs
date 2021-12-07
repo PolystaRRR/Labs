59050df2e53be96a263bb45af330d02a93dca1e4
PolystaRRR <pavel.kochetov.02@mail.ru>
2021-12-07 18:04:02 +0400

    lab2

--- Labs/part2_3/Form2.Designer.cs
+++ Labs/part2_3/Form2.Designer.cs
@@ -29,12 +29,66 @@ namespace Labs.part2_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.firstOperandTB = new System.Windows.Forms.TextBox();
+            this.operationTB = new System.Windows.Forms.TextBox();
+            this.secondOperandTB = new System.Windows.Forms.TextBox();
+            this.startCalcLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // firstOperandTB
+            // 
+            this.firstOperandTB.Location = new System.Drawing.Point(248, 196);
+            this.firstOperandTB.Name = "firstOperandTB";
+            this.firstOperandTB.Size = new System.Drawing.Size(100, 20);
+            this.firstOperandTB.TabIndex = 0;
+            // 
+            // operationTB
+            // 
+            this.operationTB.Location = new System.Drawing.Point(440, 196);
+            this.operationTB.Name = "operationTB";
+            this.operationTB.Size = new System.Drawing.Size(100, 20);
+            this.operationTB.TabIndex = 1;
+            // 
+            // secondOperandTB
+            // 
+            this.secondOperandTB.Location = new System.Drawing.Point(639, 196);
+            this.secondOperandTB.Name = "secondOperandTB";
+            this.secondOperandTB.Size = new System.Drawing.Size(100, 20);
+            this.secondOperandTB.TabIndex = 2;
+            // 
+            // startCalcLabel
+            // 
+            this.startCalcLabel.AutoSize = true;
+            this.startCalcLabel.Location = new System.Drawing.Point(459, 274);
+            this.startCalcLabel.Name = "startCalcLabel";
+            this.startCalcLabel.Size = new System.Drawing.Size(46, 13);
+            this.startCalcLabel.TabIndex = 3;
+            this.startCalcLabel.TabStop = true;
+            this.startCalcLabel.Text = "Ответ : ";
+            this.startCalcLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.startCalcLabel_LinkClicked);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1261, 635);
+            this.Controls.Add(this.startCalcLabel);
+            this.Controls.Add(this.secondOperandTB);
+            this.Controls.Add(this.operationTB);
+            this.Controls.Add(this.firstOperandTB);
+            this.Name = "Form2";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox firstOperandTB;
+        private System.Windows.Forms.TextBox operationTB;
+        private System.Windows.Forms.TextBox secondOperandTB;
+        private System.Windows.Forms.LinkLabel startCalcLabel;
     }
 }
--- Labs/part2_3/Form2.cs
+++ Labs/part2_3/Form2.cs
@@ -16,5 +16,31 @@ namespace Labs.part2_3
         {
             InitializeComponent();
         }
+        
+
+       
+
+        private void startCalcLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var firstValue = double.Parse(firstOperandTB.Text.TrimEnd().TrimStart());
+            double secondValue = double.Parse(secondOperandTB.Text.TrimEnd().TrimStart());
+
+
+            startCalcLabel.Text = operationTB.Text.TrimEnd().TrimStart() == "+" ? (firstValue + secondValue).ToString()
+            : operationTB.Text.TrimEnd().TrimStart() == "-" ? (firstValue - secondValue).ToString()
+            : operationTB.Text.TrimEnd().TrimStart() == "*" ? (firstValue * secondValue).ToString()
+            : operationTB.Text.PadLeft(0) == "/" ? (firstValue / secondValue).ToString()
+            : operationTB.Text.PadLeft(0) == "e" ? (Math.Pow(firstValue, secondValue).ToString()) : "Недопустимо";
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            firstOperandTB.Width = 150;
+            secondOperandTB.Width = 150;
+            operationTB.Width = 150;
+            startCalcLabel.Width = 200;
+            firstOperandTB.UseWaitCursor = true;
+            secondOperandTB.UseWaitCursor = true;
+        }
     }
 }
